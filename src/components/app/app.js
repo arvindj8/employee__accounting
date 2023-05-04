2c275f3066d81a6948a3f4e8aaa2d419c589cd20
Александр Буклов <arvindj8@gmail.com>
2023-05-04 21:31:25 +0300

    added loading employees from fake db on prepare props

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,6 +5,14 @@ import AppFilter from "../app-filter/app-filter";
 import EmployeesList from "../employees-list/employees-list";
 import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
+const data = [
+    {id: 1, name: 'John S.', salary: 500, increase: true},
+    {id: 2, name: 'Alex R.', salary: 5500, increase: false},
+    {id: 3, name: 'Nina E.', salary: 3400, increase: false},
+    {id: 4, name: 'Bret D.', salary: 2400, increase: false},
+]
+
+
 function App() {
     return (
         <div className="app">
@@ -14,7 +22,7 @@ function App() {
                 <AppFilter/>
             </div>
 
-            <EmployeesList/>
+            <EmployeesList data={data}/>
             <EmployeesAddForm/>
         </div>
     )
